21aa0becaf535403a144506278ba87b4835edb5c
Avishekdt15 <avishekdt15@gmail.com>
2021-01-17 18:35:19 +0530

    Initial commit

---
+++ MyUtils/src/main/java/com/example/myutils/MyMath.java
@@ -0,0 +1,10 @@
+package com.example.myutils;
+
+public class MyMath {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static int minus(int a,int b){
+        return a-b;
+    }
+}
